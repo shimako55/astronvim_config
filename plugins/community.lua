6276b64e28ccf6f20aaceec443bd550e31503ef4
sakamoto <sakamoto@sakamotonoMacBook-Pro.local>
2023-07-15 14:23:53 +0900

    chore

--- plugins/community.lua
+++ plugins/community.lua
@@ -84,8 +84,8 @@ return {
       require("indent_blankline").setup {
         space_char_blankline = " ",
         show_current_context = true,
-        -- show_current_context_start = true,
         show_end_of_line = true,
+        -- show_current_context_start = true,
       }
     end,
   },
