d29817ae25db8a1f61f2eb3ab715aa797b8d1fbb
sakamoto <sakamoto@sakamotonoMacBook-Pro.local>
2023-07-27 19:49:07 +0900

    add chatgpt.nvim

--- plugins/user.lua
+++ plugins/user.lua
@@ -10,4 +10,33 @@ return {
       require("lsp_signature").setup()
     end,
   },
+  -- ChatGPT
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup({
+        openai_params = {
+          model = "gpt-4",
+          frequency_penalty = 0,
+          presence_penalty = 0,
+          max_tokens = 300,
+          temperature = 0,
+          top_p = 1,
+          n = 1,
+        },
+        openai_edit_params = {
+          model = "code-davinci-edit-002",
+          temperature = 0,
+          top_p = 1,
+          n = 1,
+        },
+      })
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+  },
 }
