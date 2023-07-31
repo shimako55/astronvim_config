9a7bf66aa8abd465d4222eddfa51680d75ab69e0
sakamoto <sakamoto@sakamotonoMacBook-Pro.local>
2023-07-31 21:07:57 +0900

    fix lsp config yam schema setting

--- init.lua
+++ init.lua
@@ -52,20 +52,31 @@ return {
     servers = {
       -- "pyright"
     },
-    -- ["server-settings"] = {
-    --   -- example for addings schemas to yamlls
-    --   yamlls = { -- override table for require("lspconfig").yamlls.setup({...})
-    --     settings = {
-    --       yaml = {
-    --         schemas = {
-    --           ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
-    --           ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-    --           ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*.{yml,yaml}",
-    --         },
-    --       },
-    --     },
-    --   },
-    -- },
+    config = {
+      -- example for addings schemas to yamlls
+      yamlls = { -- override table for require("lspconfig").yamlls.setup({...})
+        settings = {
+          yaml = {
+            schemas = {
+              -- kubernetes = "*.yaml",
+              -- ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+              -- ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+              -- ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+              -- ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+              -- ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+              -- ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+              -- ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+              -- ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+              -- ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+              -- ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+              -- ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+              -- ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "*.k8s.yaml",
+              ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*workflow*.yaml",
+            },
+          },
+        },
+      },
+    },
   },
 
   -- Configure require("lazy").setup() options
